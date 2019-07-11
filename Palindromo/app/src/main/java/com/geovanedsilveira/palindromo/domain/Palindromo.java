f5128628fb42f742f543f6a6ef001a51d2cd004b
Geoguitar <geovanedsilveira@gmail.com>
2019-07-11 15:45:00 -0300

    created,implemented domain class

---
+++ Palindromo/app/src/main/java/com/geovanedsilveira/palindromo/domain/Palindromo.java
@@ -0,0 +1,18 @@
+package com.geovanedsilveira.palindromo.domain;
+
+public class Palindromo {
+    private String conteudo;
+
+    public Palindromo(String c){
+        this.conteudo = c;
+    }
+
+    public boolean ehPalindromo(){
+        String invertido = new StringBuilder(conteudo).reverse().toString();
+        return invertido.equalsIgnoreCase(conteudo);
+    }
+
+    public String getConteudo() {
+        return conteudo;
+    }
+}
